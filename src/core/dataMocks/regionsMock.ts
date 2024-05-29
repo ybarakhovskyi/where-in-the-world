8e509f77b49bb4a97a4787be7727d39bce15e53b
Yaroslav Barakhovskyi <yaroslav.barakhovskyi@pandadoc.com>
2024-05-29 17:00:35 +0300

    feat: add and update some mock data;

---
+++ src/core/dataMocks/regionsMock.ts
@@ -0,0 +1,7 @@
+export const regionOptionsMock = [
+  { label: 'Africa', value: 'africa' },
+  { label: 'America', value: 'america' },
+  { label: 'Asia', value: 'asia' },
+  { label: 'Europe', value: 'europe' },
+  { label: 'Oceania', value: 'oceania' },
+];
